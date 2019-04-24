4904843f658805a27d22d11c03ff73521ffaa25e
Robert Roos <roroos@abo.fi>
2019-04-24 15:40:20 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,7 +27,7 @@ def move(index,token)
   board[index]=token
 end
 
-def position_taken(index)
+def position_taken(board,index)
 if board[index]== " "
 return false
 else
@@ -36,7 +36,7 @@ end
 end
 
 def valid_move(index)
-if position_taken(index)==false && 0 < index < 9
+if position_taken(board,index)==false && 0 < index < 9
 return true
 else
 return false
